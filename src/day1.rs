49b6504e976705a63f0389f25288e8ed62c3ebdc
Florian Nagel <florian.nagel@senacor.com>
2020-12-01 06:12:46 +0100

    Finished day 1

--- src/day1.rs
+++ src/day1.rs
@@ -5,45 +5,64 @@ pub fn generator_part1(input: &str) -> Vec<isize> {
 
 #[aoc(day1, part1)]
 pub fn solve_part1(input: &[isize]) -> isize {
-    input.iter().map(|item| calculate_fuel(item)).sum()
+    find_in_vec1(input)
 }
 
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &[isize]) -> isize {
-    input
-        .iter()
-        .map(|item| calculate_fuel_recursively(*item))
-        .sum()
+    find_in_vec2(input)
 }
 
-pub fn calculate_fuel(x: &isize) -> isize {
-    ((*x as f32) / 3f32) as isize - 2
-}
-
-pub fn calculate_fuel_recursively(x: isize) -> isize {
-    let mut sum: isize = 0;
-    let mut curr_value: isize = x;
-
-    while calculate_fuel(&curr_value) > 0 {
-        curr_value = calculate_fuel(&curr_value);
-        sum += curr_value;
+pub fn find_in_vec1(input: &[isize]) -> isize {
+    for (i, x) in input.iter().enumerate() {
+        for (ii, y) in input.iter().enumerate() {
+            if i == ii {
+                continue;
+            }
+            if x + y == 2020 {
+                return x * y;
+            }
+        }
     }
+    return 0;
+}
 
-    sum
+pub fn find_in_vec2(input: &[isize]) -> isize {
+    for (i, x) in input.iter().enumerate() {
+        for (ii, y) in input.iter().enumerate() {
+            if i == ii {
+                continue;
+            }
+            for (iii, z) in input.iter().enumerate() {
+                if i == iii || i == ii {
+                    continue;
+                }
+                if x + y + z == 2020 {
+                    return x * y * z;
+                }
+            }
+        }
+    }
+    return 0;
 }
 
 #[cfg(test)]
 pub mod tests {
-    use crate::day1::{calculate_fuel_recursively, solve_part1};
+    use crate::day1::{find_in_vec1, find_in_vec2};
 
     #[test]
-    fn day1_mass() {
-        assert_eq!(solve_part1(vec![12].as_slice()), 2);
+    fn test01() {
+        assert_eq!(
+            find_in_vec1(vec![1721, 979, 366, 299, 675, 1456].as_slice()),
+            514579
+        );
     }
 
     #[test]
-    fn day1_part2() {
-        assert_eq!(calculate_fuel_recursively(14), 2);
-        assert_eq!(calculate_fuel_recursively(100756), 50346);
+    fn test02() {
+        assert_eq!(
+            find_in_vec2(vec![1721, 979, 366, 299, 675, 1456].as_slice()),
+            241861950
+        );
     }
 }
